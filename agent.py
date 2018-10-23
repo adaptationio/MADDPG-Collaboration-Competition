e26804f21d950ee70432a9961a5edc9d8a7d7929
adatationio <willie@adaptation.io>
2018-10-23 19:19:24 +1030

    chorse:cleanup comments

--- agent.py
+++ agent.py
@@ -9,14 +9,6 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-#BUFFER_SIZE = int(1e6)  # replay buffer size
-#BATCH_SIZE = 1024     # minibatch size
-#GAMMA = 0.99            # discount factor
-#TAU = 1e-3              # for soft update of target parameters
-#LR_ACTOR = 1e-4         # learning rate of the actor 
-#LR_CRITIC = 1e-3        # learning rate of the critic
-#WEIGHT_DECAY = 0        # L2 weight decay
-
 BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 1024     # minibatch size
 GAMMA = 0.99            # discount factor
